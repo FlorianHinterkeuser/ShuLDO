23518f06cf2fcd3de2f4c7fb3f875e35bc2f362f
Florian <hinterkeuser@physik.uni-bonn.de>
2017-01-25 16:14:02 +0100

    ENH: plotting WIP

--- Plot.py
+++ Plot.py
@@ -17,15 +17,23 @@ from basil.dut import Dut
 
 class Plot(object):
     
+    dir = os.getcwd()
+    
     def __init__(self, nf):
-        self.x = []
-        self.y = []
-        self.z = []
-        self.v = []
-        self.w = []
+        self.x = [[]]
+        self.y = [[]]
+        self.z = [[]]
+        self.v = [[]]
+        self.w = [[]]
+        self.V_in = []
+        self.V_out1 = []
+        self.V_out2 = []
+        self.I_in = []
+        self.I_load1 = []
+        self.result = []
         self.nf = nf
     
-    def Readout(self, Vin, Iin, Vout1, *file):
+    def Readout(self, Vin, Iin, Vout1, file):
         nf = len(file)
         self.x = [[] for x in range(0, nf)]
         self.y = [[] for y in range(0, nf)]
@@ -61,7 +69,7 @@ class Plot(object):
                         self.z[i].append(row[Vout1])
                         self.v[i].append(row[Iload1])
                     elif 'Regulator 1 load current [A]' not in csvfilearray[0] and 'Regulator 2 output voltage[V]' in csvfilearray[0]:
-                        Vout2 = 3
+                        Vout2 = 2
                         self.x[i].append(row[Vin])
                         self.y[i].append(row[Iin])
                         self.z[i].append(row[Vout1])
@@ -75,23 +83,104 @@ class Plot(object):
         for i in range(0, nf):
             pass
         self.result = [self.x, self.y, self.z, self.v, self.w]
-        #=======================================================================
-        # return {'x': x, 'y': y, 'z': z, 'v': v, 'w': w}
-        #=======================================================================
+        #print self.result
+        
         return self.result
         
-    def output(self, Vin, Iin, Vout1, *file):
-        #print self.result
-        print Plot().Readout(Vin, Iin, Vout1, *file)[0][0]
+    def Create_Axes(self, Vin, Iin, Vout1, Vout2, file, label):
         
-    def Allemalmalen_mann(self, Vin, Iin, Vout1, *file):
-        x = Plot(len(file)).Readout(Vin, Iin, Vout1, *file)[0]
-        y = Plot(len(file)).Readout(Vin, Iin, Vout1, *file)[1]
-        z = Plot(len(file)).Readout(Vin, Iin, Vout1, *file)[2]
-        v = Plot(len(file)).Readout(Vin, Iin, Vout1, *file)[3]
-        w = Plot(len(file)).Readout(Vin, Iin, Vout1, *file)[4]
-        print Plot(len(file)).nf
-        print Plot(len(file)).x
+        
+        self.Readout(Vin, Iin, Vout1, file)                                     #L2C Issue 
+        print self.result[0]
+        print self.result[4]
+        self.V_in = self.result[0]
+        self.I_in = self.result[1]
+        self.V_out1 = self.result[2]
+        self.I_load1 = self.result[3]
+        self.V_out2 = self.result[4]
+        
+        Output1 = self.V_out1
+        for i in range(0, len(file)):
+            for j in range(0, len(self.V_out1[i])):
+                if float(self.V_out1[i][j]) > 1000:
+                    self.V_out1[i][j] = float(self.V_out1[i][j])/1000
+                    if float(self.V_out1[i][j]) < 1:
+                        raise RuntimeError
+                        
+        #=======================================================================
+        # for i in range(0, len(file)):
+        #     plt.figure(1)
+        #     plt.plot(I_in[i], V_out1[i], label = label[i])
+        #     plt.grid()
+        #     plt.legend(loc='lower right')
+        #     plt.xlabel('Input current [A]')
+        #     plt.ylabel('Voltage [V]')
+        #     plt.title('Line Regulation during Irradiation')
+        #=======================================================================
+
+    def LineReg_Curr_Irradiated(self, mode, *files):
+        path = '\Messungen\Line Regulation' + str(mode)
+        os.chdir(Plot.dir + path)
+        
+        file = []
+        dirList = os.listdir('.')
+        
+        for sFile in dirList:
+            if sFile.find('.csv') == -1:
+                continue
+            file.append(sFile)
+        file = tuple(file) 
+            
+            
+        label = [None] * len(file)
+        filename = [None] * len(file)
+        dose = [None] * len(file)    
+        for i in range(0, len(file)):
+            filename[i] = file[i].split("_")[1]
+            dose[i] = filename[i].split(".c")[0]
+            label[i] = dose[i]
+               
+        
+        
+        self.Create_Axes(0, 1, 3, 2, file, label)
+        for i in range(0, len(file)):
+            plt.figure(1)
+            plt.plot(self.I_in[i], self.V_out1[i], label = label[i])
+            plt.grid()
+            plt.xlabel("Input current [A]")
+            plt.ylabel("Voltage [V]")
+            plt.title("Line Regulation during irradiation")
+            plt.axis([0,0.7,0,1.3])
+            plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+         
+        plt.show()
+        
+    def LoadReg_Curr_Irradiated(self, mode, *files):
+        path = '\Messungen\Line Regulation' + str(mode)
+        os.chdir(Plot.dir + path)
+        
+        file = []
+        dirList = os.listdir('.')
+        
+        for sFile in dirList:
+            if sFile.find('.csv') == -1:
+                continue
+            file.append(sFile)
+        file = tuple(file) 
+            
+            
+        label = [None] * len(file)
+        filename = [None] * len(file)
+        dose = [None] * len(file)    
+        for i in range(0, len(file)):
+            filename[i] = file[i].split("_")[1]
+            dose[i] = filename[i].split(".c")[0]
+            label[i] = dose[i]
+               
+        print label       
+        #self.Create_Axes(0, , Vout1, Vout2, file, label)
+
+        plt.show()
         
         
         
@@ -121,18 +210,10 @@ class Plot(object):
                 plots[i] = csv.reader(csvfile[i], delimiter=',')
                 next(plots[i], None)
                 for row in plots[i]:
-                    #print row
                     x[i].append(row[0])
                     y[i].append(row[1])
  
-        #print plots
-         
-            #plt.figure(z)
-            #ax = plt.subplot(111)
             plt.plot(x[i], y[i], color[i], label = label[i])
-          #  plt.plot(x[i], v[i], color[i] + '--', label = '2, ' + label[i])
-             
-        #plt.set_position([box.x0, box.y0, box.width * 0.8, box.height])
         plt.legend(loc='lower right')
         plt.xlabel('Reference voltage [V]')
         plt.ylabel('Output voltage [V]')
@@ -159,14 +240,6 @@ class Plot(object):
         for i in range(0, nf):
             csvfile[i] = 'csvfile' + str(i)
         
-        #=======================================================================
-        # path = os.getcwd()
-        # if pfad not in path:
-        #     path = os.getcwd() + r'\Messungen' + pfad
-        # os.chdir(path)
-        # print os.getcwd()
-        # print nf
-        #=======================================================================
         for i in range(0, nf):
             print os.getcwd()
             with open(file[i], 'rb') as csvfile[i]:
@@ -174,21 +247,12 @@ class Plot(object):
                 next(plots[i], None)
                 next(plots[i], None)
                 for row in plots[i]:
-                    #print row
                     x[i].append(row[Iin])
                     y[i].append(row[Vout])
                     v[i].append(row[Vin])
-                    #w[i].append(row[Vout2])
- 
-        #print plots
-         
-            #plt.figure(z)
-            #ax = plt.subplot(111)
+
         plt.plot(x[0], y[0], color[0], label = 'Iin vs Vout, ' + label[0])
         plt.plot(x[0], v[0], color[1] + '--', label = 'Iin vs Vin' + label[0])
-        #plt.plot(x[0], w[0], color[1], label = 'Iin vs Vout, ' + label[1])
-             
-        #plt.set_position([box.x0, box.y0, box.width * 0.8, box.height])
         plt.legend(loc='lower right')
         plt.xlabel('Input current [A]')
         plt.ylabel('Voltage [V]')
@@ -196,60 +260,7 @@ class Plot(object):
         plt.grid()
         plt.axis([0,0.95,0,2])
         plt.tight_layout()
-        #plt.show()
         plt.savefig('CMode_LineReg_Vref_500mV.pdf')
-        
-        
-    
-    
-    def Lineregulation_VOLT(self, pfad, Vref, z, *file):
-        
-        nf = len(file)
-        x = [[] for x in range(0, nf)]
-        y = [[] for y in range(0, nf)]
-        csvfile = [None]*len(file)
-        plots = [None]*len(file)
-        label = ['Iload = 0 mA', 'Iload = 250 mA', 'Iload = 500 mA']
-
-        
-        for i in range(0, nf):
-            csvfile[i] = 'csvfile' + str(i)
-        print csvfile
-        
-        path = os.getcwd()
-        if pfad not in path:
-            path = os.getcwd() + r'\Messungen' + pfad
-        os.chdir(path)
-        print os.getcwd()
-        
-        for i in range(0, nf):
-            print file[i]
-            with open(file[i], 'rb') as csvfile[i]:
-                plots[i] = csv.reader(csvfile[i], delimiter=',')
-                next(plots[i], None)
-                for row in plots[i]:
-                    #print row
-                    x[i].append(row[0])
-                    y[i].append(row[2])
-                print x[i]
-                print y[i]
-
-        #print plots
-        
-            plt.figure(z)
-            #ax = plt.subplot(111)
-            plt.plot(x[i], y[i], label = label[i])
-            
-        #plt.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-        plt.legend(loc='lower right')
-        plt.xlabel('Input voltage [V]')
-        plt.ylabel('Output voltage [V]')
-        plt.title('Line Regulation, Vref = ' + str(Vref) + ' mV')
-        plt.grid()
-        plt.axis([1.2,2,0.7,1.4])
-        plt.tight_layout()
-        #plt.show()
-        plt.savefig('VMode_LineReg_Vref' + str(Vref) +'mV.pdf')
         
         
     def Loadregulation_CURR(self, pfad, z, *file):
@@ -281,144 +292,30 @@ class Plot(object):
                 next(plots[i], None)
                 next(plots[i], None)
                 for row in plots[i]:
-                    #print row
                     x[i].append(row[3])
                     y[i].append(row[2])
                     v[i].append(row[0])
-                    #v[i].append(row[1])
-
-        #print plots
         
             plt.figure(z)
-            #ax = plt.subplot(111)
             plt.plot(x[i], y[i], color[i], label = 'ILoad vs Vout, ' + label[i])
             plt.plot(x[i], v[i], color[i] + '--', label = 'Iload vs Vin, ' + label[i])
             
-        #plt.set_position([box.x0, box.y0, box.width * 0.8, box.height])
         plt.legend(loc='best')
         plt.xlabel('Load current [A]')
         plt.ylabel('Output voltage [V]')
         plt.title('Iin = 0.5 A, Iload vs. Vout & Iload vs. Vin')
         plt.grid()
         plt.axis([0,-0.45, 0.6, 1.6])
-        #plt.tight_layout()
-        #plt.show()
-        #plt.gca().invert_xaxis()
         plt.savefig('CMode_LoadReg.pdf')
         
-    def Loadregulation_CURR_parallel(self, pfad, z, *file):
-        
-        nf = len(file)
-        x = [[] for x in range(0, nf)]
-        y = [[] for y in range(0, nf)]
-        v = [[] for v in range(0,nf)]
-        csvfile = [None]*len(file)
-        plots = [None]*len(file)
-        label = ['Vref = 500 mV', 'Vref = 600 mV', 'Vref = 660 mV']
-        color = ['r', 'b', 'g']
-
-        
-        for i in range(0, nf):
-            csvfile[i] = 'csvfile' + str(i)
-        print csvfile
-        
-        path = os.getcwd()
-        if pfad not in path:
-            path = os.getcwd() + r'\Messungen' + pfad
-        os.chdir(path)
-        print os.getcwd()
-        
-        for i in range(0, nf):
-            print file[i]
-            with open(file[i], 'rb') as csvfile[i]:
-                plots[i] = csv.reader(csvfile[i], delimiter=',')
-                next(plots[i], None)
-                next(plots[i], None)
-                for row in plots[i]:
-                    #print row
-                    x[i].append(row[3])
-                    y[i].append(row[2])
-                    v[i].append(row[0])
-                    #v[i].append(row[1])
-
-        #print plots
-        
-            plt.figure(z)
-            #ax = plt.subplot(111)
-            plt.plot(x[i], y[i], color[i], label = 'ILoad vs Vout, ' + label[i])
-            plt.plot(x[i], v[i], color[i] + '--', label = 'Iload vs Vin, ' + label[i])
-            
-        #plt.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-        plt.legend(loc='best')
-        plt.xlabel('Load current [A]')
-        plt.ylabel('Output voltage [V]')
-        plt.title('Iin = 1 A, Iload vs. Vout & Iload vs. Vin')
-        plt.grid()
-        plt.axis([0,-0.45, 0.6, 1.6])
-        #plt.tight_layout()
-        #plt.show()
-        #plt.gca().invert_xaxis()
-        plt.savefig('CMode_LoadReg_parallel.pdf')    
-    
-    def Loadregulation_VOLT(self, pfad, Vref, z, *file):
-        
-        nf = len(file)
-        x = [[] for x in range(0, nf)]
-        y = [[] for y in range(0, nf)]
-        csvfile = [None]*len(file)
-        plots = [None]*len(file)
-        label = ['Vref = 500 mV', 'Vref = 600 mV', 'Vref = 660 mV']
-
-        
-        for i in range(0, nf):
-            csvfile[i] = 'csvfile' + str(i)
-        print csvfile
-        
-        path = os.getcwd()
-        if pfad not in path:
-            path = os.getcwd() + r'\Messungen' + pfad
-        os.chdir(path)
-        print os.getcwd()
-        
-        for i in range(0, nf):
-            print file[i]
-            with open(file[i], 'rb') as csvfile[i]:
-                plots[i] = csv.reader(csvfile[i], delimiter=',')
-                next(plots[i], None)
-                for row in plots[i]:
-                    x[i].append(row[3])
-                    y[i].append(row[2])
-
-        
-            plt.figure(z)
-            plt.plot(x[i], y[i], label = label[i])
-            
-        plt.legend(loc='best')
-        plt.xlabel('Load current [A]')
-        plt.ylabel('Output voltage [V]')
-        plt.title('Vin = 1.5 V, Iload vs. Vout')
-        plt.grid()
-        plt.axis([0, -0.45, 0.85, 1.4])
-        plt.tight_layout()
-        #plt.show()
-        #plt.gca().invert_xaxis()
-        plt.savefig('VMode_LoadReg.pdf')
         
 if __name__ == '__main__':
     plot = Plot(None)
     
-    #plot.Readout(0, 1, 2, 'CMode_500_mV_LineReg.csv', 'CMode_600_mV_LineReg.csv', 'CMode_660_mV_LineReg.csv', 'CMode_600_500_mV_LoadReg.csv') 
-    #print plot.Readout(0 , 1, 2, 'CMode_500_mV_LineReg.csv', 'CMode_600_mV_LineReg.csv', 'CMode_660_mV_LineReg.csv', 'CMode_600_500_mV_LoadReg.csv')
-    #print plot.output(plot.Readout, 0 , 1, 2, 'CMode_500_mV_LineReg.csv', 'CMode_600_mV_LineReg.csv', 'CMode_660_mV_LineReg.csv', 'CMode_600_500_mV_LoadReg.csv')
-    #print plot.Readout(0 , 1, 2, 'CMode_500_mV_LineReg.csv', 'CMode_600_mV_LineReg.csv', 'CMode_660_mV_LineReg.csv', 'CMode_600_500_mV_LoadReg.csv')[0][0]
-    #print plot.output(0 , 1, 2, 'CMode_500_mV_LineReg.csv', 'CMode_600_mV_LineReg.csv', 'CMode_660_mV_LineReg.csv', 'CMode_600_500_mV_LoadReg.csv')
-    plot.Allemalmalen_mann(0, 1, 2, 'CMode_500_mV_LineReg.csv', 'CMode_600_mV_LineReg.csv', 'CMode_660_mV_LineReg.csv', 'CMode_600_500_mV_LoadReg.csv')
-    #plot.Lineregulation_CURR(660, 1, 0, 2, 3, 'CMode_500_mV_LineReg.csv')
-    #plot.Loadregulation_VOLT('\Load Regulation\Voltage Supply', 500, 1, 'VMode_500_mV_LoadReg.csv', 'VMode_600_mV_LoadReg.csv', 'VMode_660_mV_LoadReg.csv')
-    #plot.Loadregulation_CURR('\Load Regulation\Current Supply', 1, 'CMode_500_mV_LoadReg.csv', 'CMode_600_mV_LoadReg.csv', 'CMode_660_mV_LoadReg.csv')
-    #plot.Loadregulation_CURR_parallel('\Load Regulation\Current Supply', 1, 'CMode_600_500_mV_LoadReg_parallel2.csv', 'CMode_500_600_mV_LoadReg_parallel2.csv')
-    #plot.Readout(r'\Messungen\Line Regulation\Current Supply', 'CMode_600_mV_LineReg.csv', 'CMode_500_mV_LineReg.csv', 'CMode_600_500_mV_LineReg_parallel.csv')
-    #plot.VrefVout('\VrefVout', 'VrefVout_Curr.csv', 'VrefVout_Volt.csv')
+
+    plot.LineReg_Curr_Irradiated('\Current Supply')
+
+
 
 
     
