d9285635c4d45544577938f641a9b68260b1e18e
Florian <hinterkeuser@physik.uni-bonn.de>
2017-01-30 17:02:31 +0100

    ENH: plotting WIP

--- Plot.py
+++ Plot.py
@@ -7,12 +7,16 @@ import time
 import logging
 import csv
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import multiprocessing
 import progressbar
 import os.path
 import numpy as np
 
 from basil.dut import Dut
+import matplotlib.colors as mcolors
+import matplotlib.cm as cm
+from matplotlib import _cm_listed as cmap
 
 
 class Plot(object):
@@ -32,6 +36,7 @@ class Plot(object):
         self.I_load1 = []
         self.result = []
         self.nf = nf
+        self.plasma = cmap._plasma_data
     
     def Readout(self, Vin, Iin, Vout1, file):
         nf = len(file)
@@ -43,38 +48,41 @@ class Plot(object):
         csvfilearray = [[] for csvfilearray in range(0, nf)]
         csvfile = [None] * nf
         plots = [None] * nf
-        label = []
-        color = []
+
+        
+        
         for i in range(0, nf):
             csvfile[i] = 'csvfile' + str(i)
             
         for i in range(0,nf):
             with open(file[i], 'rb') as csvfile[i]:
                 plots[i] = csv.reader(csvfile[i], delimiter=',')
-                next(plots[i], None)
+                
+                header = next(plots[i]) 
+                
                 for row in plots[i]:
                     csvfilearray[i].append(row)
-                    if 'Regulator 2 output voltage[V]' in csvfilearray[0] and 'Regulator 1 load current [A]' in csvfilearray[0]:
-                        Iload1 = 3
-                        Vout2 = 4
+                    if 'Regulator 2 output voltage [V]' in header and 'Regulator 1 load current [A]' in header:
+                        Iload1 = 0
+                        Vout2 = 1
                         self.x[i].append(row[Vin])
                         self.y[i].append(row[Iin])
                         self.z[i].append(row[Vout1])
                         self.v[i].append(row[Iload1])
                         self.w[i].append(row[Vout2])
-                    elif 'Regulator 1 load current [A]' in csvfilearray[0] and 'Regulator 2 output voltage[V]' not in csvfilearray[0]:
+                    elif 'Regulator 1 load current [A]' in header and 'Regulator 2 output voltage [V]' not in header:
                         Iload1 = 3
                         self.x[i].append(row[Vin])
                         self.y[i].append(row[Iin])
                         self.z[i].append(row[Vout1])
                         self.v[i].append(row[Iload1])
-                    elif 'Regulator 1 load current [A]' not in csvfilearray[0] and 'Regulator 2 output voltage[V]' in csvfilearray[0]:
+                    elif 'Regulator 1 load current [A]' not in header and 'Regulator 2 output voltage [V]' in header:
                         Vout2 = 2
                         self.x[i].append(row[Vin])
                         self.y[i].append(row[Iin])
                         self.z[i].append(row[Vout1])
                         self.w[i].append(row[Vout2])                    
-                    elif 'Regulator 1 load current [A]' not in csvfilearray[0] and 'Regulator 2 output voltage[V]' not in csvfilearray[0]:
+                    elif 'Regulator 1 load current [A]' not in header and 'Regulator 2 output voltage [V]' not in header:
                         self.x[i].append(row[Vin])
                         self.y[i].append(row[Iin])
                         self.z[i].append(row[Vout1])
@@ -83,21 +91,21 @@ class Plot(object):
         for i in range(0, nf):
             pass
         self.result = [self.x, self.y, self.z, self.v, self.w]
-        #print self.result
         
         return self.result
         
-    def Create_Axes(self, Vin, Iin, Vout1, Vout2, file, label):
+    def Create_Axes(self, Vin, Iin, Vout1, file, label):
         
         
-        self.Readout(Vin, Iin, Vout1, file)                                     #L2C Issue 
-        print self.result[0]
-        print self.result[4]
+        self.Readout(Vin, Iin, Vout1, file)                                     #L2C Issue
         self.V_in = self.result[0]
         self.I_in = self.result[1]
         self.V_out1 = self.result[2]
         self.I_load1 = self.result[3]
         self.V_out2 = self.result[4]
+        self.color = [None]*len(file)
+
+        
         
         Output1 = self.V_out1
         for i in range(0, len(file)):
@@ -106,17 +114,17 @@ class Plot(object):
                     self.V_out1[i][j] = float(self.V_out1[i][j])/1000
                     if float(self.V_out1[i][j]) < 1:
                         raise RuntimeError
-                        
-        #=======================================================================
-        # for i in range(0, len(file)):
-        #     plt.figure(1)
-        #     plt.plot(I_in[i], V_out1[i], label = label[i])
-        #     plt.grid()
-        #     plt.legend(loc='lower right')
-        #     plt.xlabel('Input current [A]')
-        #     plt.ylabel('Voltage [V]')
-        #     plt.title('Line Regulation during Irradiation')
-        #=======================================================================
+                    
+        
+        for i in range(0, len(file)):
+            self.scale = len(self.plasma)/len(file)*i
+            self.color[i] = self.plasma[self.scale]
+        
+            
+
+            
+            
+
 
     def LineReg_Curr_Irradiated(self, mode, *files):
         path = '\Messungen\Line Regulation' + str(mode)
@@ -142,21 +150,23 @@ class Plot(object):
                
         
         
-        self.Create_Axes(0, 1, 3, 2, file, label)
+        self.Create_Axes(0, 1, 3, file, label)
         for i in range(0, len(file)):
             plt.figure(1)
-            plt.plot(self.I_in[i], self.V_out1[i], label = label[i])
+            plt.plot(self.I_in[i], self.V_out2[i], label = label[i], alpha=0.5)
+            #plt.plot(self.I_in[i], self.V_out1[i], label = label[i], alpha=0.5)
+            #plt.plot(self.I_in[i], self.V_in[i], label = label[i], alpha=0.5)
             plt.grid()
             plt.xlabel("Input current [A]")
             plt.ylabel("Voltage [V]")
             plt.title("Line Regulation during irradiation")
-            plt.axis([0,0.7,0,1.3])
+            plt.axis([0,0.7,0,1.9])
             plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
          
-        plt.show()
+        #plt.show()
         
     def LoadReg_Curr_Irradiated(self, mode, *files):
-        path = '\Messungen\Line Regulation' + str(mode)
+        path = '\Messungen\Load Regulation' + str(mode)
         os.chdir(Plot.dir + path)
         
         file = []
@@ -167,6 +177,8 @@ class Plot(object):
                 continue
             file.append(sFile)
         file = tuple(file) 
+        
+        print sorted(file)
             
             
         label = [None] * len(file)
@@ -176,11 +188,55 @@ class Plot(object):
             filename[i] = file[i].split("_")[1]
             dose[i] = filename[i].split(".c")[0]
             label[i] = dose[i]
+                
                
-        print label       
-        #self.Create_Axes(0, , Vout1, Vout2, file, label)
-
+        
+        
+        self.Create_Axes(Vin = 3, Iin= 4, Vout1= 5, file = file, label = label)
+        
+        for i in range(0, len(file)):            
+            
+            fig1 = plt.figure(1)
+            ax1 = fig1
+            plt.plot(self.I_load1[i], self.V_out2[i], label = label[i], alpha=0.8, color = self.color[i])
+            plt.xlabel('Load current [A]')
+            plt.ylabel("Voltage [V]")
+            plt.grid(True)
+            plt.title("Vout2 vs. Iload")
+            plt.axis([0,0.42,1.19,1.204])
+            #plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)  
+            
+            
+                   
+            fig2 = plt.figure(2)
+            ax2 = fig2
+            plt.plot(self.I_load1[i], self.V_out1[i], label = label[i], alpha=0.8, color = self.color[i])
+            plt.xlabel('Load current [A]')
+            plt.ylabel("Voltage [V]")
+            plt.grid(True)
+            plt.title("Vout1 vs. Iload")
+            plt.axis([0,0.42,1.165,1.21])
+             
+            fig3 = plt.figure(3)
+            ax3=fig3
+            plt.plot(self.I_load1[i], self.V_in[i], label = label[i], alpha=0.8, color = self.color[i])
+            plt.xlabel('Load current [A]')
+            plt.ylabel("Voltage [V]")
+            plt.grid(True)
+            plt.title("Vin vs. Iload")
+            plt.axis([0,0.42,1.34,1.5])
+        sm = plt.cm.ScalarMappable(cmap = 'plasma', norm = plt.Normalize(vmin=0, vmax=500))
+        sm._A = []
+        cbar1= ax1.colorbar(sm)
+        cbar1.set_label('Mrad', rotation = 270)
+        cbar2=ax2.colorbar(sm)
+        cbar2.set_label('Mrad', rotation = 270)
+        cbar3=ax3.colorbar(sm)        
+        cbar3.set_label('Mrad', rotation = 270)
         plt.show()
+        plt.close('all')
+
+
         
         
         
@@ -312,10 +368,8 @@ class Plot(object):
 if __name__ == '__main__':
     plot = Plot(None)
     
-
-    plot.LineReg_Curr_Irradiated('\Current Supply')
-
-
+    #plot.LineReg_Curr_Irradiated('\Current Supply')
+    plot.LoadReg_Curr_Irradiated('\Current Supply')
 
 
     
