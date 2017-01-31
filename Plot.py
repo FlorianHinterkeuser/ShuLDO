d75b9fcd3a65ed805368cf538937804bbb2c55e8
Florian <hinterkeuser@physik.uni-bonn.de>
2017-01-31 16:44:23 +0100

    ENH: plotting WIP

--- Plot.py
+++ Plot.py
@@ -8,15 +8,13 @@ import logging
 import csv
 import matplotlib.pyplot as plt
 import matplotlib as mpl
-import multiprocessing
-import progressbar
 import os.path
 import numpy as np
-
-from basil.dut import Dut
 import matplotlib.colors as mcolors
 import matplotlib.cm as cm
 from matplotlib import _cm_listed as cmap
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 
 
 class Plot(object):
@@ -36,7 +34,7 @@ class Plot(object):
         self.I_load1 = []
         self.result = []
         self.nf = nf
-        self.plasma = cmap._plasma_data
+        self.plasma = cmap._inferno_data
     
     def Readout(self, Vin, Iin, Vout1, file):
         nf = len(file)
@@ -94,7 +92,7 @@ class Plot(object):
         
         return self.result
         
-    def Create_Axes(self, Vin, Iin, Vout1, file, label):
+    def Create_Axes(self, Vin, Iin, Vout1, file):
         
         
         self.Readout(Vin, Iin, Vout1, file)                                     #L2C Issue
@@ -117,7 +115,7 @@ class Plot(object):
                     
         
         for i in range(0, len(file)):
-            self.scale = len(self.plasma)/len(file)*i
+            self.scale = int(len(self.plasma)/len(file))*i
             self.color[i] = self.plasma[self.scale]
         
             
@@ -138,33 +136,77 @@ class Plot(object):
                 continue
             file.append(sFile)
         file = tuple(file) 
-            
-            
-        label = [None] * len(file)
-        filename = [None] * len(file)
-        dose = [None] * len(file)    
-        for i in range(0, len(file)):
-            filename[i] = file[i].split("_")[1]
-            dose[i] = filename[i].split(".c")[0]
-            label[i] = dose[i]
-               
         
         
-        self.Create_Axes(0, 1, 3, file, label)
+        self.Create_Axes(Vin = 0, Iin = 1, Vout1 = 3, file=file)
+        
+        
         for i in range(0, len(file)):
-            plt.figure(1)
-            plt.plot(self.I_in[i], self.V_out2[i], label = label[i], alpha=0.5)
-            #plt.plot(self.I_in[i], self.V_out1[i], label = label[i], alpha=0.5)
-            #plt.plot(self.I_in[i], self.V_in[i], label = label[i], alpha=0.5)
-            plt.grid()
+            fig1 = plt.figure(1)
+            ax1=fig1
+            plt.plot(self.I_in[i], self.V_out2[i], color = self.color[i], alpha=0.8)
+            plt.grid(True)
+            plt.xlabel("Input Current [A]")
+            plt.ylabel("Regulator 2 Output Voltage [V]")
+            plt.title("Iin vs. Vout2")
+            plt.axis([0.,0.7,0.,1.25])
+            
+            fig2 = plt.figure(2)
+            ax2=fig2
+            plt.plot(self.I_in[i], self.V_out1[i], color = self.color[i], alpha=0.8)
+            plt.grid(True)
             plt.xlabel("Input current [A]")
-            plt.ylabel("Voltage [V]")
-            plt.title("Line Regulation during irradiation")
+            plt.ylabel("Regulator 1 output voltage [V]")
+            plt.title("Iin vs. Vout1")
+            plt.axis([0,0.7,0,1.25])
+            
+            fig3 = plt.figure(3)
+            ax3=fig3
+            plt.plot(self.I_in[i], self.V_in[i], color = self.color[i], alpha=0.8)
+            plt.grid(True)
+            plt.xlabel("Input current [A]")
+            plt.ylabel("Input voltage [V]")
+            plt.title("Iin vs. Vin")
             plt.axis([0,0.7,0,1.9])
-            plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-         
-        #plt.show()
+            
+        sm = plt.cm.ScalarMappable(cmap = 'plasma', norm = plt.Normalize(vmin=0, vmax=500))
+        sm._A = []
+        cbar1= fig1.colorbar(sm)
+        cbar1.set_label('Total Dose [Mrad]', rotation = 270, labelpad=20)
+        cbar2=fig2.colorbar(sm)
+        cbar2.set_label('Total Dose [Mrad]', rotation = 270, labelpad = 20)
+        cbar3=fig3.colorbar(sm)        
+        cbar3.set_label('Total Dose [Mrad]', rotation = 270, labelpad = 20)
         
+        ax1 = fig1.add_subplot(111)                                                     #Setting up subplots for zooming
+        ax2 = fig2.add_subplot(111)
+        
+        axins1 = zoomed_inset_axes(ax1, 2, bbox_to_anchor=(267,167))
+        axins2 = zoomed_inset_axes(ax2, 2, bbox_to_anchor=(307,167))
+        
+        
+        axins1.grid(False)
+        axins2.grid(False)
+        axins1.set_xlim(0.35,0.45)
+        axins2.set_xlim(0.4,0.5)
+        axins1.set_ylim(1.13, 1.24)
+        axins2.set_ylim(1.13,1.24)
+        
+        for i in range(0, len(file)):
+            axins1.plot(self.I_in[i], self.V_out2[i], color = self.color[i], alpha=0.8)
+            axins2.plot(self.I_in[i], self.V_out1[i], color = self.color[i], alpha=0.8)
+        mark_inset(ax1, axins1, 2,1, fc="none", ec= "0")
+        mark_inset(ax2, axins2, 2, 1, fc="none", ec="0")
+        
+        
+        
+        plt.figure(1).savefig("IinVout2.pdf")
+        plt.figure(2).savefig("IinVout1.pdf")
+        plt.figure(3).savefig("InVin.pdf")
+        plt.close('all')
+        
+        
+
     def LoadReg_Curr_Irradiated(self, mode, *files):
         path = '\Messungen\Load Regulation' + str(mode)
         os.chdir(Plot.dir + path)
@@ -192,47 +234,41 @@ class Plot(object):
                
         
         
-        self.Create_Axes(Vin = 3, Iin= 4, Vout1= 5, file = file, label = label)
+        self.Create_Axes(Vin = 3, Iin= 4, Vout1= 5, file = file)
         
         for i in range(0, len(file)):            
             
             fig1 = plt.figure(1)
-            ax1 = fig1
             plt.plot(self.I_load1[i], self.V_out2[i], label = label[i], alpha=0.8, color = self.color[i])
             plt.xlabel('Load current [A]')
             plt.ylabel("Voltage [V]")
             plt.grid(True)
             plt.title("Vout2 vs. Iload")
-            plt.axis([0,0.42,1.19,1.204])
-            #plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)  
-            
-            
+            plt.axis([0,0.42,1.19,1.205]) 
                    
             fig2 = plt.figure(2)
-            ax2 = fig2
             plt.plot(self.I_load1[i], self.V_out1[i], label = label[i], alpha=0.8, color = self.color[i])
             plt.xlabel('Load current [A]')
             plt.ylabel("Voltage [V]")
             plt.grid(True)
             plt.title("Vout1 vs. Iload")
-            plt.axis([0,0.42,1.165,1.21])
+            plt.axis([0,0.42,1.16,1.21])
              
             fig3 = plt.figure(3)
-            ax3=fig3
             plt.plot(self.I_load1[i], self.V_in[i], label = label[i], alpha=0.8, color = self.color[i])
             plt.xlabel('Load current [A]')
             plt.ylabel("Voltage [V]")
             plt.grid(True)
             plt.title("Vin vs. Iload")
-            plt.axis([0,0.42,1.34,1.5])
+            plt.axis([0,0.42,1.3,1.5])
         sm = plt.cm.ScalarMappable(cmap = 'plasma', norm = plt.Normalize(vmin=0, vmax=500))
         sm._A = []
-        cbar1= ax1.colorbar(sm)
-        cbar1.set_label('Mrad', rotation = 270)
-        cbar2=ax2.colorbar(sm)
-        cbar2.set_label('Mrad', rotation = 270)
-        cbar3=ax3.colorbar(sm)        
-        cbar3.set_label('Mrad', rotation = 270)
+        cbar1= fig1.colorbar(sm)
+        cbar1.set_label('Total Dose [Mrad]', rotation = 270, labelpad = 20)
+        cbar2=fig2.colorbar(sm)
+        cbar2.set_label('Total Dose [Mrad]', rotation = 270, labelpad = 20)
+        cbar3=fig3.colorbar(sm)        
+        cbar3.set_label('Total Dose [Mrad]', rotation = 270, labelpad = 20)
         plt.show()
         plt.close('all')
 
@@ -278,92 +314,9 @@ class Plot(object):
         plt.axis([0.4,0.66,0.8,1.32])
         plt.tight_layout()
         plt.savefig('VrefVout.pdf')
-            
-    
-    def Lineregulation_CURR(self, Vref, Iin, Vin, Vout, Vout2, *file):
-        
-        nf = len(file)
-        x = [[] for x in range(0, nf)]
-        y = [[] for y in range(0, nf)]
-        v = [[] for v in range(0, nf)]
-        w = [[] for w in range(0, nf)]
-        csvfile = [None]*len(file)
-        plots = [None]*len(file)
-        label = ['Vref = 600 mV', 'Vref = 500 mV']
-        color = ['r', 'b', 'g']
 
-        
-        for i in range(0, nf):
-            csvfile[i] = 'csvfile' + str(i)
-        
-        for i in range(0, nf):
-            print os.getcwd()
-            with open(file[i], 'rb') as csvfile[i]:
-                plots[i] = csv.reader(csvfile[i], delimiter=',')
-                next(plots[i], None)
-                next(plots[i], None)
-                for row in plots[i]:
-                    x[i].append(row[Iin])
-                    y[i].append(row[Vout])
-                    v[i].append(row[Vin])
 
-        plt.plot(x[0], y[0], color[0], label = 'Iin vs Vout, ' + label[0])
-        plt.plot(x[0], v[0], color[1] + '--', label = 'Iin vs Vin' + label[0])
-        plt.legend(loc='lower right')
-        plt.xlabel('Input current [A]')
-        plt.ylabel('Voltage [V]')
-        plt.title('Line Regulation, two parallel ShuLDOs')
-        plt.grid()
-        plt.axis([0,0.95,0,2])
-        plt.tight_layout()
-        plt.savefig('CMode_LineReg_Vref_500mV.pdf')
-        
-        
-    def Loadregulation_CURR(self, pfad, z, *file):
-        
-        nf = len(file)
-        x = [[] for x in range(0, nf)]
-        y = [[] for y in range(0, nf)]
-        v = [[] for v in range(0,nf)]
-        csvfile = [None]*len(file)
-        plots = [None]*len(file)
-        label = ['Vref = 500 mV', 'Vref = 600 mV', 'Vref = 660 mV']
-        color = ['r', 'b', 'g']
 
-        
-        for i in range(0, nf):
-            csvfile[i] = 'csvfile' + str(i)
-        print csvfile
-        
-        path = os.getcwd()
-        if pfad not in path:
-            path = os.getcwd() + r'\Messungen' + pfad
-        os.chdir(path)
-        print os.getcwd()
-        
-        for i in range(0, nf):
-            print file[i]
-            with open(file[i], 'rb') as csvfile[i]:
-                plots[i] = csv.reader(csvfile[i], delimiter=',')
-                next(plots[i], None)
-                next(plots[i], None)
-                for row in plots[i]:
-                    x[i].append(row[3])
-                    y[i].append(row[2])
-                    v[i].append(row[0])
-        
-            plt.figure(z)
-            plt.plot(x[i], y[i], color[i], label = 'ILoad vs Vout, ' + label[i])
-            plt.plot(x[i], v[i], color[i] + '--', label = 'Iload vs Vin, ' + label[i])
-            
-        plt.legend(loc='best')
-        plt.xlabel('Load current [A]')
-        plt.ylabel('Output voltage [V]')
-        plt.title('Iin = 0.5 A, Iload vs. Vout & Iload vs. Vin')
-        plt.grid()
-        plt.axis([0,-0.45, 0.6, 1.6])
-        plt.savefig('CMode_LoadReg.pdf')
-        
         
 if __name__ == '__main__':
     plot = Plot(None)
