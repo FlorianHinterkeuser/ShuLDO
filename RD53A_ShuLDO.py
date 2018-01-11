4750490b03d2df5aceb374c874510d280acffe7a
florianhinterkeuser <hinterkeuser@physik.uni-bonn.de>
2018-01-11 12:41:50 +0100

    RD53A testing preparation

--- RD53A_ShuLDO.py
+++ RD53A_ShuLDO.py
@@ -18,7 +18,7 @@ class IV(object):
     '''
     Class to perform a standard IV scan of the FE65 ShuLDO.
     '''    
-    def scan_IinVinVout_CURR(self, file_name, max_Iin, inputPolarity, steps, stepSize, Vref, Voff):
+    def scan_IinVinVout_CURR(self, file_name, max_Iin, inputPolarity, steps, stepSize):
         '''
         IV-scan in current supply mode.
         '''
@@ -28,34 +28,23 @@ class IV(object):
 
         misc.reset(1, 'Sourcemeter1')   #Iin
         misc.reset(2, 'Sourcemeter1')   #Iload
-        misc.reset(1, 'Sourcemeter2')   #Vref
-        misc.reset(1, 'Sourcemeter3')   #Voff
 
         misc.set_source_mode('CURR', 1, 'Sourcemeter1')
-        misc.set_source_mode('CURR', 2, 'Sourcemeter1')
-        misc.set_source_mode('VOLT', 1, 'Sourcemeter2')
-        misc.set_source_mode('VOLT', 1, 'Sourcemeter3')        
+        misc.set_source_mode('CURR', 1, 'Sourcemeter2')       
 
         dut['Sourcemeter1'].four_wire_on(channel=1)
-        dut['Sourcemeter1'].four_wire_off(channel=2)
 
         dut['Sourcemeter1'].set_voltage_limit(2, channel = 1)
-        dut['Sourcemeter1'].set_voltage_limit(2, channel = 2)
-        dut['Sourcemeter2'].set_current_limit(0.01)
-        dut['Sourcemeter3'].set_current_limit(0.01)        
+        dut['Sourcemeter2'].set_voltage_limit(2)    
 
         dut['Sourcemeter1'].set_current_range(3, channel = 1)
-        dut['Sourcemeter1'].set_current_range(3, channel = 2)
         dut['Sourcemeter2'].set_autorange()
-        dut['Sourcemeter3'].set_autorange()
 
-        dut['Sourcemeter2'].set_voltage(Vref)
-        dut['Sourcemeter3'].set_voltage(Voff)
+        dut['Sourcemeter1'].set_current(0, channel=1)
+        dut['Sourcemeter2'].set_current(0)
 
         dut['Sourcemeter1'].on(channel=1)
-        dut['Sourcemeter1'].on(channel=2)
         dut['Sourcemeter2'].on()
-        dut['Sourcemeter3'].on()
 
         fncounter=1
         while os.path.isfile(file_name):
@@ -65,7 +54,7 @@ class IV(object):
 
         with open(file_name, 'wb') as outfile:
             f = csv.writer(outfile, quoting=csv.QUOTE_NONNUMERIC)
-            f.writerow(['Input current [A]', 'Input voltage [V]', 'Output voltage [V]', 'Reference Voltage [V]', 'Offset Voltage [V]'])
+            f.writerow(['Input current [A]', 'Input voltage [V]', 'Output voltage [V]'])
 
             inputCurr = 0
             for x in range(0, int(steps)):
@@ -73,19 +62,17 @@ class IV(object):
 
                 input_current = inputCurr
                 input_voltage = misc.measure_voltage(1, 'Sourcemeter1')[0]
-                output_voltage = misc.measure_voltage(2, 'Sourcemeter1')[0]
-                reference_voltage = misc.measure_voltage(1, 'Sourcemeter2')[0]
-                offset_voltage = misc.measure_voltage(1, 'Sourcemeter3')[0]
+                output_voltage = misc.measure_voltage(1, 'Sourcemeter2')[0]
 
                 logging.info("Input current is %r A" % input_current)
                 logging.info("Input voltage is %r V" % input_voltage)
                 logging.info("Regulator output voltage is %r V" % output_voltage)
-                logging.info("Regulator reference voltage ratio is %r" % reference_voltage)
-                logging.info("Regulator offset voltage ratio is %r" % offset_voltage)
 
-                misc.data.append([input_current, input_voltage, output_voltage, reference_voltage, offset_voltage])
+                misc.data.append([input_current, input_voltage, output_voltage])
                 f.writerow(misc.data[-1])
-
+                
+                if inputCurr >= 0.5:
+                    stepSize = 0.0001
                 inputCurr += stepSize
                 if float(inputCurr) > float(max_Iin) or float(input_voltage) >= 1.99:
                     break
@@ -95,7 +82,6 @@ class IV(object):
         misc.reset(1, 'Sourcemeter1')
         misc.reset(2, 'Sourcemeter1')
         misc.reset(1, 'Sourcemeter2')
-        misc.reset(1, 'Sourcemeter3')
         iv.livePlot(file_name)
 
     def livePlot(self, file_name):
@@ -104,7 +90,7 @@ class IV(object):
         file = []
         
         with open(file_name, 'rb') as csvfile:
-            plots = csv.reader(csvfile, delimiter=',')
+            plots = csv.reader(csvfile, delimiter=';')
             header = next(plots)
             for row in plots:
                 csvfilearray.append(row)
@@ -115,32 +101,31 @@ class IV(object):
             elif "Input current" in header[i]:
                 Iin = i
                 logging.info("Input current in column %r" % i)
-            elif "output voltage" in header[i]:                         #Beware: lower case "o" in output voltage!
-                Vout1 = i
+            elif "Output voltage" in header[i]:                         #Beware: lower case "o" in output voltage!
+                Vout = i
                 logging.info("Output voltage in column %r" % i)
         
         
         V_in = [None]*len(csvfilearray)
         I_in = [None]*len(csvfilearray)
-        V_out1 = [None]*len(csvfilearray)
-        V_drop1 = [None]*len(csvfilearray)
+        V_out = [None]*len(csvfilearray)
         
         
         for i in range(0, len(csvfilearray)):
             try:
-                V_in[i] = csvfilearray[i][Vin]
-                I_in[i] = csvfilearray[i][Iin]
-                V_out1[i] = csvfilearray[i][Vout1]
+                V_in[i] = float(csvfilearray[i][Vin])
+                I_in[i] = float(csvfilearray[i][Iin])
+                V_out[i] = float(csvfilearray[i][Vout])
             except AttributeError:
                 pass
-            V_drop1[i] = float(V_in[i]) - float(V_out1[i])
         
         
-        plt.grid(True)
         
-        plt.plot(I_in, V_out1, ".-", markersize=3, linewidth=0.5, color = 'r', label= 'Output Voltage')
+        #plt.grid(True)
+        plt.plot(I_in, V_out, ".-", markersize=3, linewidth=0.5, color = 'r', label= 'Output Voltage')
         plt.plot(I_in, V_in, ".-", markersize=3, linewidth=0.5, color = 'b', label = 'Input Voltage')
-        plt.axis([0,2.0,0,2.0])
+        plt.axis("auto")
+
         plt.xlabel('Input Current / A')
         plt.ylabel('Voltage / V')
         
@@ -154,10 +139,12 @@ if __name__ == '__main__':
     dut = Dut('devices.yaml')
     dut.init()
     misc = Misc(dut=dut)
-
+  
     print 'Sourcemeter 1: '+dut['Sourcemeter1'].get_name()
     print 'Sourcemeter 2: '+dut['Sourcemeter2'].get_name()
 
-
+    os.chdir(os.getcwd() + "\output")
     iv = IV()
-    fileName = "output/IV_CURR/IV_CURR_Vref_600mV_Voff_800mV.csv"
+    fileName = "output/RD53A.csv"
+    #iv.scan_IinVinVout_CURR(fileName, 2, 1, 5500, 0.01)
+    iv.livePlot("RD53A_Vout.csv")
