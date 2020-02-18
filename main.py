80fdf27490904ec3ece028c46b85537c67b333e5
Florian Hinterkeuser <hinterkeuser@physik.uni-bonn.de>
2020-02-18 13:42:00 +0100

    Quick Cleanup

--- main.py
+++ main.py
@@ -6,31 +6,10 @@ Created on 26.09.2016
 
 import os.path
 import time
-#import SLDO as SLDO
-#import plot
 from basil.dut import Dut
 import SLDO as SLDO
-import Xray_auto as XRay
 
 
-#===============================================================================
-# def measure(run_number = 0):
-# #    run_number = 0 #SET +1 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-#     
-#     flavor='IV' #switch Vin!!
-#     filepath = "/Xray_warm/"  + "/" + flavor
-#     
-#     if not os.path.exists(os.path.normpath(os.getcwd() + filepath)):
-#         os.makedirs(os.path.normpath(os.getcwd() + filepath))
-#     os.chdir(os.path.normpath(os.getcwd() + filepath))
-#     
-#     iv = SLDO.IV()
-#     iv.measure_temp()
-#     iv.scan_IV("IV", 1.2, 1, 50, 0.025, run_number=run_number, remote_sense=False)
-#     time.sleep(5)
-#     iv.scan_load_reg("LoadReg", 0.6, 0.6, 50, 0.025, run_number = run_number)
-#     iv.working_point()
-#===============================================================================
 def measure(run_number = 0):
     dut.init()
     
@@ -43,25 +22,13 @@ def measure(run_number = 0):
 
     dut.close()
     
-def irradiate(current = 51, timer = 60):
-    machine.init()
-    xray = XRay.Xray(dut = machine)
-
-    xray.set_tube(current, timer)
-
-    machine.close()
-    
 dut = Dut('devices.yaml')
-machine = Dut("xray.yaml")
 
 flavor='IV' #switch Vin!!
-filepath = "/Xray_warm/"  + "/" + flavor
+filepath = 'output'
 
 if not os.path.exists(os.path.normpath(os.getcwd() + filepath)):
     os.makedirs(os.path.normpath(os.getcwd() + filepath))
 os.chdir(os.path.normpath(os.getcwd() + filepath))
-#measure(run_number= 2)
 
-
-irradiate(51, 2640)
-measure(24)
+measure(0)
